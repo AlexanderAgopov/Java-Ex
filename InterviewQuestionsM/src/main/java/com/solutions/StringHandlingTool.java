f3b75b0368cb4ef937b5b8657d12ac2b453ec1fb
AlexanderAgopov <alexander.agopov@gmail.com>
2019-01-10 18:20:26 +0200

    Add answers to string FAQ

--- InterviewQuestionsM/src/main/java/com/solutions/StringHandlingTool.java
+++ InterviewQuestionsM/src/main/java/com/solutions/StringHandlingTool.java
@@ -1,6 +1,11 @@
 package com.solutions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 /**
  * This class contains methods that represent answers to frequently asked string
@@ -73,25 +78,104 @@ public class StringHandlingTool {
 	 * @return
 	 */
 	public char getFirstNonRepeatedCharUsingHashMap(String s) {
-		HashMap<Character, Integer> scoreboard = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 		char ch = ' ';
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
-			if (scoreboard.containsKey(c)) {
-				scoreboard.put(c, scoreboard.get(c) + 1);
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
 			} else {
-				scoreboard.put(c, 1);
+				map.put(c, 1);
 			}
 		}
 
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
-			if (scoreboard.get(c) == 1) {
+			if (map.get(c) == 1) {
 				ch = c;
 				break;
 			}
 		}
 		return ch;
+	}
+
+	/**
+	 * This method checks if a string contains only digits.
+	 * 
+	 * @param s - the string
+	 * @return true if the string contains only digits
+	 */
+	public boolean areAllCharsDigitsUsingForLoop(String s) {
+		boolean digits = true;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) < 0 || s.charAt(i) > 9) {
+				digits = false;
+			}
+		}
+		return digits;
+	}
+
+	public HashSet<Character> getAllDuplicateChars(String s) {
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < s.length(); i++) {
+			if (map.containsKey(s.charAt(i))) {
+				map.replace(s.charAt(i), map.get(s.charAt(i)) + 1);
+			} else {
+				map.put(s.charAt(i), 1);
+			}
+		}
+		HashSet<Character> duplicateChars = new HashSet<Character>();
+		for (int i = 0; i < s.length(); i++) {
+			if (map.get(s.charAt(i)) >= 2) {
+				duplicateChars.add(s.charAt(i));
+			}
+		}
+		return duplicateChars;
+	}
+
+	public int[] countVowelsAndConsonants(String s) {
+		int vowels = 0;
+		int consonants = 0;
+		int[] values = { vowels, consonants };
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o'
+					|| s.charAt(i) == 'u' || s.charAt(i) == 'y') {
+				vowels++;
+			} else {
+				consonants++;
+			}
+		}
+		return values;
+	}
+
+	public int countCharOccurrences(String s, char c) {
+		int counter = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == c) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+
+	public String reverseWordsInSentenceUsingWhileLoop(String sentence) {
+		String reversedSentence = "";
+		while (sentence.lastIndexOf(' ') != -1) {
+			reversedSentence = reversedSentence + sentence.substring(sentence.lastIndexOf(' ')) + " ";
+			sentence = sentence.substring(0, sentence.lastIndexOf(' '));
+		}
+		return reversedSentence;
+	}
+
+	public boolean isPalindrome(String s) {
+		boolean palindrome = true;
+		for (int i = 0; i < s.length() / 2; i++) {
+			if (s.charAt(i) != s.length() - 1 - s.charAt(i)) {
+				palindrome = false;
+				break;
+			}
+		}
+		return palindrome;
 	}
 
 }
